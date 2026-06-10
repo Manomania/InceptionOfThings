183f5afdcfe7e2ee973434ce216974380f7a097c
maximart <maximart@maximart.maximart>
2026-06-10 18:31:23 +0200

    feat: add part two. deployment is good. needs to add service and ingress for apps

---
+++ p2/scripts/server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "[LOG] Update package and install 'curl'"
+apt-get update -y && apt-get install curl -y
+
+if ! command -v k3s &> /dev/null; then
+	echo "[LOG] Installing k3s"
+	curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110" sh -
+fi
+
+echo "[LOG] Deploying application one"
+kubectl apply -f /vagrant/confs/app1/
+
+echo "[LOG] Deploying application two"
+kubectl apply -f /vagrant/confs/app2/
+
+echo "[LOG] Deploying application three"
+kubectl apply -f /vagrant/confs/app3/
